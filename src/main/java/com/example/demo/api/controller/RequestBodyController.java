80ab93346ddcd86f0326a2cfcf68ff3aa4eda7f3
SionNoh <vmfpsel@abc.ne.kr>
2023-08-20 22:03:08 +0900

    RequestBodyController update

--- src/main/java/com/example/demo/api/controller/RequestBodyController.java
+++ src/main/java/com/example/demo/api/controller/RequestBodyController.java
@@ -1,6 +1,9 @@
 package com.example.demo.api.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +11,10 @@ import com.example.demo.api.dto.ApiDTO;
 
 @RestController
 public class RequestBodyController {
+	
+	public void name(String name) {
+		System.out.println("name === " + name);
+	}
 
 	@GetMapping("/getbody")
 	public String getbody(
@@ -16,7 +23,59 @@ public class RequestBodyController {
 		String strResult = "{ \"result\":\"FAIL\" }";
 		
 		try {
+			String name = apiDTO.getName();
+			name(name);
+			strResult = "{ \"result\":\"OK\" }";
+		} catch(Exception e) {
 			
+		}
+		return strResult;
+	}
+	
+	@PostMapping("/postbody")
+	public String postbody(
+			@RequestBody ApiDTO apiDTO) {
+		
+		String strResult = "{ \"result\":\"FAIL\" }";
+		
+		try {
+			
+			String name = apiDTO.getName();
+			name(name);
+			strResult = "{ \"result\":\"OK\" }";
+		} catch(Exception e) {
+			
+		}
+		return strResult;
+	}
+	
+	@PutMapping("/putbody")
+	public String putbody(
+			@RequestBody ApiDTO apiDTO) {
+		
+		String strResult = "{ \"result\":\"FAIL\" }";
+		
+		try {
+			
+			String name = apiDTO.getName();
+			name(name);
+			strResult = "{ \"result\":\"OK\" }";
+		} catch(Exception e) {
+			
+		}
+		return strResult;
+	}
+	
+	@DeleteMapping("/deletebody")
+	public String deletebody(
+			@RequestBody ApiDTO apiDTO) {
+		
+		String strResult = "{ \"result\":\"FAIL\" }";
+		
+		try {
+			
+			String name = apiDTO.getName();
+			name(name);
 			strResult = "{ \"result\":\"OK\" }";
 		} catch(Exception e) {
 			
